af12bab6581c1cfeea3603068089830d03c1bfd0
HarisShaikh97 <harisahmedsheikh1997@gmail.com>
2024-07-30 03:01:24 +0500

    Context action removed...

--- context/reducer.js
+++ context/reducer.js
@@ -1,8 +1,5 @@
 export const reducer = (state, action) => {
 	switch (action.type) {
-		case "SET_ACCESS_TOKEN": {
-			return { ...state, accessToken: action?.payload }
-		}
 		case "SET_SIGNUP_USER": {
 			return { ...state, signupUser: action?.payload }
 		}
